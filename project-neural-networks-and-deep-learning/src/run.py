76cf000b8f7263807ba52c2bf526972883e3b447
XingxingHuang <phoebe800@gmail.com>
2017-02-28 16:12:48 +0800

    modified run.py

--- project-neural-networks-and-deep-learning/src/run.py
+++ project-neural-networks-and-deep-learning/src/run.py
@@ -47,3 +47,8 @@ import network2
 net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
 net.large_weight_initializer()
 net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True)
+
+
+# http://neuralnetworksanddeeplearning.com/chap5.html
+# vanishing gradient problem
+# exploding gradient problem
